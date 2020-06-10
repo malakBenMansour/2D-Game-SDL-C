10946a5852e7101acc07296b39a5deb941cf83cd
waelbelgaied <65318369+waelbelgaied@users.noreply.github.com>
2020-06-10 05:30:01 -0700

    doxygene wael

---
+++ doxygene/doxygene/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['background',['background',['../structbackground.html',1,'']]],
+  ['background_2ec',['background.c',['../background_8c.html',1,'']]],
+  ['background_2eh',['background.h',['../background_8h.html',1,'']]]
+];
---
+++ doxygene/main.c
@@ -0,0 +1,51 @@
+/**
+* @file main.c
+* @author wael
+* @date Mai 14, 2020
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "background.h"
+int main (int argc, char *argv[]){
+background b;
+SDL_Event event;
+SDL_Surface *screen;
+if (SDL_Init (SDL_INIT_VIDEO) == -1) {
+printf ("Can't init SDL: %s\n", SDL_GetError ());
+return EXIT_FAILURE;
+}
+screen = SDL_SetVideoMode (CAMERA_W, CAMERA_H, 32,
+SDL_HWSURFACE | SDL_DOUBLEBUF);
+initBckg (&b, "background.png");
+showBckg (screen, b);
+SDL_Flip (screen);
+int continuer = 0;
+SDL_EnableKeyRepeat (SDL_DEFAULT_REPEAT_DELAY,
+SDL_DEFAULT_REPEAT_INTERVAL);
+while (!continuer) {
+while (SDL_PollEvent (&event)){
+switch (event.type) {
+case SDL_QUIT:
+continuer = 1;
+break;
+case SDL_KEYDOWN:
+switch (event.key.keysym.sym)
+{
+case SDLK_LEFT:
+scrollToLeft (&b);
+break;
+case SDLK_RIGHT:
+scrollToRight (&b);
+break;
+}
+break;
+}
+}
+showBckg (screen, b);
+SDL_Flip (screen);
+}
+SDL_FreeSurface(screen);
+SDL_FreeSurface(b.img);
+}
