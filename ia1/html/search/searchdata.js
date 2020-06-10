cb027e2b18c6e0c5d9e2cfc6901455b949754529
aziz123-ui <aziz.benzarti@esprit.tn>
2020-06-10 12:45:53 +0200

    intelligence artificielle de l'ennemi_aziz

---
+++ ia1/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "cimo",
+  1: "co",
+  2: "im"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files"
+};
+
